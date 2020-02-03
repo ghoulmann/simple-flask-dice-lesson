f7f7847dfaa3fd99038132d7fc123f5a3cc3c900
Rik Goldman <rikgoldman@gmail.com>
2020-02-03 04:48:26 -0500

    missing dice, coin flip, integrate with routes

--- app.py
+++ app.py
@@ -7,33 +7,42 @@ def hello_world():
 @app.route('/d20')
 def d20():
     a = Die(20)
-    return a.roll
+    return a.result
 @app.route('/d100')
 def d100():
     a = Die(100)
-    return a.roll
+    return a.result
 @app.route('/d4')
 def d4():
     a = Die(4)
-    return a.roll
+    return a.result
 @app.route('/d6')
 def d6():
     a = Die(6)
-    return a.roll
+    return a.result
 @app.route('/2d6')
 def d6ab():
     a = Die(6)
     b = Die(6)
-    return a.roll + ", " + b.roll
+    together = int(a.result) + int(b.result)
+    return a.result + ", " + b.result + ": " + str(together)
 @app.route('/d50')
 def d50():
     a = Die(50)
-    return a.roll
+    return a.result
 @app.route('/d8')
 def d8():
     a = Die(8)
-    return a.roll
+    return a.result
 @app.route('/d10')
 def d10():
     a = Die(10)
-    return a.roll
+    return a.result
+@app.route('/d12')
+def d12():
+    a = Die(12)
+    return a.result
+@app.route('/coin')
+def coin():
+    a = Die(2)
+    return a.result
--- die.py
+++ die.py
@@ -1,4 +1,16 @@
-from random import randint
+from random import randint, choice
+from datetime import datetime
 class Die(object):
     def __init__(self, size):
-        self.roll = str(randint(1, size))
+        self.size = size
+        self.timestamp = datetime.now()
+        self.result = self.roll(self.size)
+    def roll(self, size):
+        if size % 2 == 0:
+            if size == 2:
+                return choice(['heads', 'tails']) 
+            return str(randint(1, size))
+        else:
+            raise ValueError
+
+
